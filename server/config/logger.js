e118133df20a794e291ddcbbbb4f76ce616f537f
Francesco <fran.gramano@mail.utoronto.ca>
2018-03-04 15:51:00 -0500

    Automatically creates log file if it doesn't exist

--- server/config/logger.js
+++ server/config/logger.js
@@ -1,14 +1,26 @@
 import winston from 'winston';
+import fs from 'fs';
+import path from 'path';
 
 winston.emitErrs = true;
+
+const logDir = path.resolve(
+    path.dirname(__filename),'..', '..', 'logs'
+);
+
+// Generally we don't like doing synchronous things with Node, BUT since this will only happen once, when
+// file gets used for the first time, it's okay
+if (!fs.existsSync(logDir)) {
+    fs.mkdirSync(logDir);
+}
 
 let logger = new winston.Logger({
     transports: [
         new winston.transports.File({
             level: 'info',
-            filename: './logs/all-logs.log',
+            filename: path.join(logDir, 'all-logs.log'),
             handleExceptions: true,
-            json: true,
+            json: false,
             maxsize: 5242880, // 5MB limit
             maxFiles: 5,
             colorize: false
