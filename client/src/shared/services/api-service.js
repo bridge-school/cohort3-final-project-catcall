563d9ab4d6a05e8e497c73993928cc114d20c717
Francesco <fran.gramano@mail.utoronto.ca>
2018-03-07 19:43:40 -0500

    Uses env variable for API port

--- client/src/shared/services/api-service.js
+++ client/src/shared/services/api-service.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 class ApiService {
     constructor() {
         return axios.create({
-            baseURL: `${process.env.REACT_APP_API_HOST}/api`,
+            baseURL: `${process.env.REACT_APP_API_HOST}:${process.env.REACT_APP_API_PORT}/api`,
             timeout: 1000,
         });
     }
