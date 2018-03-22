854d2268e03a96dbb4a17b55d6de7fc5f15b66df
Stephanie Zeng <stephaniezeng521@gmail.com>
2018-03-22 16:34:29 -0400

    Feature: Add router - Configure redux-logger to debug - Add router reducer - Extract MainPage component from App component - Add routes to App component

---
+++ client/src/containers/MainPage.js
@@ -0,0 +1,58 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+import { getUserLocation, fetchLocation } from '../actions/index';
+import { Link } from 'react-router-dom';
+
+import '../App.css';
+
+import Input from '../components/Input'
+import Button from '../components/Button'
+
+class MainPage extends Component {
+
+  componentDidMount() {
+    if (navigator.geolocation) {
+      this.props.fetchLocation(navigator.geolocation);
+    }
+  }
+
+  handleChange = (e) => {
+    this.props.getUserLocation(e.target.value);
+  }
+
+  startNewReport = () => { }
+
+  viewReports = () => { }
+
+  render() {
+    const { browserLocation } = this.props;
+
+    const location = browserLocation && browserLocation.latitude && browserLocation.longitude ? `${browserLocation.latitude} ${browserLocation.longitude}` : 'Loading location...';
+
+    return (
+      <div className="App">
+        <Input
+          inputValue={location}
+          handleChange={this.handleChange}
+        />
+        <Button><Link to="/report">Report Incident</Link></Button>
+        <Button><Link to="/data">View Reports</Link>
+
+        </Button>
+      </div>
+    );
+  }
+}
+
+const mapStateToProps = storeState => ({
+  userLocation: storeState.rootReducer.locationReducer.userInput,
+  browserLocation: storeState.rootReducer.locationReducer.browserLocation,
+})
+
+const mapDispatchToProps = {
+  getUserLocation,
+  fetchLocation,
+};
+
+export default connect(mapStateToProps, mapDispatchToProps)(MainPage);
