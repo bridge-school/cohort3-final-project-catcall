4d741222ddb5495e9338b68c7b5501b6a935f5d7
Purvi Kanal <purvikanal92@gmail.com>
2018-04-06 14:26:37 -0400

    remove jest from dev dependencies

--- client/src/reducers/__tests__/reducers.spec.js
+++ client/src/reducers/__tests__/reducers.spec.js
@@ -1,68 +1,69 @@
-import * as reducers from '../index';
-import { ACTION_TYPES } from '../../actions/index';
-
+import * as reducers from "../index";
+import { ACTION_TYPES } from "../../actions/index";
 
 let defaultState;
 
-describe('locationReducer', () => {
-    beforeEach(() => {
-        defaultState = {
-            browserLocation: {
-                latitude: '',
-                longitude: '',
-            },
-            userInput: 'hello',
-        };
+console.log("reducers: ", reducers);
+
+describe("locationReducer", () => {
+  beforeEach(() => {
+    defaultState = {
+      browserLocation: {
+        latitude: "",
+        longitude: ""
+      },
+      userInput: "hello"
+    };
+  });
+
+  test("should return default state when type doesn't match", () => {
+    expect.assertions(1);
+
+    expect(reducers.locationReducer(undefined, {})).toEqual(defaultState);
+  });
+
+  test("should handle USER_LOCATION type", () => {
+    expect.assertions(1);
+    const location = "18 York Street";
+
+    expect(
+      reducers.locationReducer(
+        {},
+        {
+          type: ACTION_TYPES.userLocation,
+          payload: {
+            location
+          }
+        }
+      )
+    ).toEqual({
+      userInput: location
     });
+  });
 
-    test('should return default state when type doesn\'t match', () => {
-        expect.assertions(1);
+  test("should handle GET_LOCATION_SUCCESS after fetching location is done", () => {
+    expect.assertions(1);
+    const location = {
+      latitude: "43.654357",
+      longitude: "-79.4218942"
+    };
 
-        expect(reducers.locationReducer(undefined, {})).toEqual(defaultState);
+    expect(
+      reducers.locationReducer(
+        {},
+        {
+          type: ACTION_TYPES.getLocationSuccess,
+          payload: {
+            latitude: location.latitude,
+            longitude: location.longitude
+          }
+        }
+      )
+    ).toEqual({
+      browserLocation: {
+        latitude: location.latitude,
+        longitude: location.longitude
+      }
     });
-
-    test('should handle USER_LOCATION type', () => {
-        expect.assertions(1);
-        const location = '18 York Street';
-
-        expect(
-            reducers.locationReducer(
-                {},
-                {
-                    type: ACTION_TYPES.userLocation,
-                    payload: {
-                        location,
-                    }
-                }
-            )
-        ).toEqual({
-            userInput: location,
-        })
-    });
-
-    test('should handle GET_LOCATION_SUCCESS after fetching location is done', () => {
-        expect.assertions(1);
-        const location = {
-            latitude: '43.654357',
-            longitude: '-79.4218942',
-        };
-
-        expect(
-            reducers.locationReducer(
-                {},
-                {
-                    type: ACTION_TYPES.getLocationSuccess,
-                    payload: {
-                        latitude: location.latitude,
-                        longitude: location.longitude,
-                    }
-                }
-            )
-        ).toEqual({
-            browserLocation: {
-                latitude: location.latitude,
-                longitude: location.longitude,
-            }
-        })
-    });
+  });
 });
