b0f82d452ed7a7dd77371d34bfef0af04061b56f
Taly <Taly@giovannas-MacBook-Pro.local>
2018-04-06 01:48:11 -0400

    Changed Emojis bar to use our custom components, with styling

--- client/src/containers/RatingForm.js
+++ client/src/containers/RatingForm.js
@@ -3,11 +3,11 @@ import { connect } from 'react-redux';
 import { handleSubmitReport } from '../actions/index';
 import EmojiButtonContainer from './EmojiButtonContainer';
 import Button from '../components/Button';
+import { StyledGrid, StyledRow, StyledCol } from '../components/styled/StyledGridElements';
 
 const RatingForm = ({ handleSubmitReport, push, state }) => {
   
   const doSubmission = e => {
-    // e.preventDefault();
     handleSubmitReport();
     push('/data');
   }
@@ -15,17 +15,19 @@ const RatingForm = ({ handleSubmitReport, push, state }) => {
   const validForm = state.loc.lat && state.loc.lng && state.selectedRating
 
   return (
-    <div>
+    <StyledGrid>
+      <StyledRow>
       <h2>Please state how you felt when the incident happened</h2>
-      <div className="emoji-buttons">
-        <EmojiButtonContainer reaction="expressionless" />
-        <EmojiButtonContainer reaction="anguished" />
-        <EmojiButtonContainer reaction="angry" />
-        <EmojiButtonContainer reaction="fearful" />
-        <EmojiButtonContainer reaction="screaming" />
-      </div>
+      </StyledRow>
+      <StyledRow>
+        <StyledCol xs={3} sm={3} md={3} lg={3}><EmojiButtonContainer reaction="expressionless" /></StyledCol>
+        <StyledCol xs={2} sm={2} md={2} lg={2}><EmojiButtonContainer reaction="anguished" /></StyledCol>
+        <StyledCol xs={2} sm={2} md={2} lg={2}><EmojiButtonContainer reaction="angry" /></StyledCol>
+        <StyledCol xs={2} sm={2} md={2} lg={2}><EmojiButtonContainer reaction="fearful" /></StyledCol>
+        <StyledCol xs={3} sm={3} md={3} lg={3}><EmojiButtonContainer reaction="screaming" /></StyledCol>
+      </StyledRow>
       <Button bsStyle="primary" onClick={doSubmission} disabled={!validForm}>Submit</Button>
-    </div>
+    </StyledGrid>
   )
 }
 
