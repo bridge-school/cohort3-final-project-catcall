91c26dece69ca1a5ecf13b826cb015dde313e93e
Kristen <kristen.l.lutz@gmail.com>
2018-04-11 21:12:06 -0400

    found a debugger

--- client/src/containers/ViewMapContainer.js
+++ client/src/containers/ViewMapContainer.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 import ViewReportMap from '../components/ViewReportMap'
 
 export default class ViewMapContainer extends Component {
-  debugger;
   render() {
     const { reports } = this.props;
     return (
